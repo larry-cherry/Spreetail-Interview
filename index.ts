1e433c34e14fa1f7f6cc799c237d49b0b8ef1f52
Larry Cherry <larry.cherry.0406@gmail.com>
2022-02-02 08:36:35 -0600

    Updated to use a class for managing state

--- index.ts
+++ index.ts
@@ -1,11 +1,9 @@
-type Member = string[]
+type Member = Set<string>
 
 
 type MultiValueDictionary = {
   [key: string]: Member
 }
-
-let multiValueDictionary: MultiValueDictionary = {}
 
 process.stdin.setEncoding('utf8');
 
@@ -16,106 +14,212 @@ enum COMMANDS {
   CLEAR = 'CLEAR', // Removes all keys and all members from the dictionary
   EXIT = 'EXIT', // Exits Commandline App
   REMOVEALL = 'REMOVEALL', // Removes all members for a key and removes the key from the dictionary. Returns an error if the key does not exist.
+  KEYS = 'KEYS',
   KEYEXISTS = 'KEYEXISTS', // Returns whether a key exists or not
   MEMBEREXISTS = 'MEMBEREXISTS', // Returns whether a member exists within a key
   ALLMEMBERS = 'ALLMEMBERS', // Returns all the members in the dictionary
   ITEMS = 'ITEMS' // Returns all keys in the dictionary and all of their members.
 }
 
-let debug = false;
-// console.log('args', process.argv)
+type stringOrUndefined = string | undefined;
 
-process.stdin.on('data', function(data: Buffer) {
-    const input = data.toString().trim();
-    try {
-      inputParser(input);
-    } catch (error) {
-      console.log(error);
+class Spreetail {
+  multiValueDictionary: MultiValueDictionary;
+  consoleOutput = false
+  constructor(){
+    this.multiValueDictionary = {}
+  }
+
+  commandparser(cmd: stringOrUndefined, key: stringOrUndefined, member: stringOrUndefined){
+    switch(cmd){
+      case COMMANDS.ADD:
+        if(typeof key === 'string' && typeof member === 'string'){
+          this.add(key, member);
+        }
+        break;
+      case COMMANDS.REMOVE:
+        if(typeof key === 'string' && typeof member === 'string'){
+          this.remove(key, member);
+        }
+        break;
+      case COMMANDS.MEMBERS:
+        if(typeof key === 'string'){
+          this.members(key);
+        }
+        break;
+      case COMMANDS.CLEAR:
+        this.clear();
+        break;
+      case COMMANDS.REMOVEALL:
+        if(typeof key === 'string'){
+          this.removeall(key)
+        };
+        break;
+      case COMMANDS.KEYS:
+        this.keys();
+        break;
+      case COMMANDS.KEYEXISTS:
+        if(typeof key === 'string'){
+          this.printToConsole(this.keyexists(key))
+        }
+        break;
+      case COMMANDS.MEMBEREXISTS:
+        if(typeof key === 'string' && typeof member === 'string'){
+          this.printToConsole(this.membersexists(key, member))
+        }
+        break;
+      case COMMANDS.ALLMEMBERS:
+        this.allmembers();
+        break;
+      case COMMANDS.ITEMS:
+        this.items();
+        break;
+      case COMMANDS.EXIT:
+        process.exit(0)
     }
-});
+  }
+  add(key: string, member: string){
+    if(!this.keyexists(key)){
+      this.multiValueDictionary[key] = new Set();
+      this.multiValueDictionary[key].add(member);
+    } else {
+      if(!this.membersexists(key, member)){
+        this.multiValueDictionary[key].add(member);
+      } else {
+        this.printToConsole('ERROR, member already exists for key')
+      }
+    }
+  }
+  remove(key: string, member: string){
+    if(this.membersexists(key, member)){
+      this.multiValueDictionary[key].delete(member);
+      if(this.multiValueDictionary[key].size === 0){
+        this.removeall(key);
+      }
+    } else {
+      this.printToConsole('ERROR, member does not exist');
+    }
+  }
+  members(key: string){
+    if(this.keyexists(key)){
+      const members = this.multiValueDictionary[key]
+      let i = 1;
+      for(const member of members){
+        this.printToConsole(`${i}) ${member}`)
+        i++;
+      }
+      return this.multiValueDictionary[key];
+    } else {
+      this.printToConsole('ERROR, key does not exist.');
+    }
+  }
+  clear(){
+    this.multiValueDictionary = {}
+    return this.multiValueDictionary
+  }
+  removeall(key: string){
+    if(this.keyexists(key)){
+      delete this.multiValueDictionary[key];
+    } else {
+      this.printToConsole('ERROR, key does not exist')
+    }
+  }
+  keys(){
+    const keys = Object.keys(this.multiValueDictionary);
+    if(keys.length === 0){
+      this.printToConsole('empty set');
+    } else {
+      let i = 1;
+      for(const key of keys){
+        this.printToConsole(`${i}) ${key}`);
+      }
+    }
+    return keys
+  }
+  keyexists(key: string): boolean {
+    if(key in this.multiValueDictionary){
+      return true;
+    }
+    // this.printToConsole(result);
+    return false;
+  }
+  membersexists(key: string, member: string): boolean {
+    if(this.keyexists(key)){
+      return this.multiValueDictionary[key].has(member);
+    }
+    return false;
+  }
+  allmembers(){
+    const keys = Object.keys(this.multiValueDictionary);
+    const members: string[] = [];
+    let i = 1;
+    for(const key of keys){
+      for(const member of this.multiValueDictionary[key]){
+        members.push(member);
+        this.printToConsole(`${i}) ${member}`);
+        i++;
+      }
+    }
+    return members;
+  }
+  items(): void{
+    const keys = Object.keys(this.multiValueDictionary);
+    let i = 1;
+    for(const key of keys){
+      for(const member of this.multiValueDictionary[key]){
+        this.printToConsole(`${i}) ${key}: ${member}`);
+        i++;
+      }
+    }
+  }
+  printToConsole(msg: any){
+    if(this.consoleOutput) {
+      console.log(msg);
+    }
+  }
+} 
 
-export function inputParser(input: string) {
+let debug = false;
+
+
+type inputParserReturn = {
+  cmd: stringOrUndefined,
+  key: stringOrUndefined,
+  member: stringOrUndefined
+}
+
+function inputParser(input: string): inputParserReturn{
   const inputArray  = input.split(' ');
   if(debug){
     console.log('inputArray', inputArray);
   }
   const [cmd, key] = inputArray;
-  const val = inputArray.slice(2, inputArray.length).join(' ');
-  const allMemeberKeys = Object.keys(multiValueDictionary);
-  switch(cmd){
-    case COMMANDS.ADD:
-      console.log('adding')
-      if(key !== undefined && val !== undefined){
-        if (key in multiValueDictionary){
-          if(multiValueDictionary[key].includes(val)){
-            throw new Error(`ERROR, ${val} already exists for ${key}`)
-          } else {
-            multiValueDictionary[key].push(val);
-          }
-        } else {
-          multiValueDictionary[key] = [val];
-        }
-      }
-      break;
-    case COMMANDS.MEMBERS:
-      if(key in multiValueDictionary){
-        for(const member of multiValueDictionary[key]){
-          console.log(member);
-        }
-      } else {
-        throw new Error(`${key} does not exist`);
-      }
-      break;
-    case COMMANDS.MEMBEREXISTS:
-      if(key in multiValueDictionary){
-        console.log(multiValueDictionary[key].includes(val));
-      }
-      break;
-    case COMMANDS.ALLMEMBERS:
-      let allmemebers: string[] = [];
-      // const allMemeberKeys = Object.keys(multiValueDictionary);
-      for(const memberKey of allMemeberKeys){
-        multiValueDictionary[memberKey].forEach(memberVal => {
-          console.log(memberVal)
-        })
-      }
-      break;
-    case COMMANDS.KEYEXISTS:
-      if(key in multiValueDictionary){
-        console.log(true);
-      } else {
-        console.log(false);
-      }
-    case COMMANDS.REMOVE:
-      if(key in multiValueDictionary){
-        const newMembers = multiValueDictionary[key].filter(member => member != val);
-        if(newMembers.length > 0){
-          multiValueDictionary[key] = newMembers;
-        } else {
-          delete multiValueDictionary[key];
-        }
-      }
-      break;
-    case COMMANDS.REMOVEALL:
-      if(key in multiValueDictionary){
-        multiValueDictionary[key] = []
-      } else {
-        throw new Error(`${key} does not exist`);
-      }
-      break;
-    case COMMANDS.ITEMS:
-      for(const memberKey of allMemeberKeys){
-        console.log('key', memberKey);
-        multiValueDictionary[memberKey].forEach(memberVal => {
-          console.log(memberVal)
-        })
-      }
-      break;
-    case COMMANDS.EXIT:
-      process.exit(0);
-    case COMMANDS.CLEAR:
-      multiValueDictionary={}
-      break;
-  }
+  const member = inputArray.slice(2, inputArray.length).join(' ');
+  return {
+    cmd,
+    key,
+    member
+  } 
 }
 
+
+if (require.main === module) {
+  let spreetail = new Spreetail();
+  spreetail.consoleOutput = true
+  process.stdin.on('data', function(data: Buffer) {
+    const input = data.toString().trim();
+    if(spreetail !== null){
+      try {
+        const {cmd, key, member} = inputParser(input);
+        spreetail.commandparser(cmd, key, member);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+});
+}
+
+export {
+  Spreetail,
+  inputParser
+}
